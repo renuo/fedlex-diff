d2c17c491db77b99f2f5e2e7c67a4654732eadf3
nicole schmidlin <nicole.schmidlin@renuo.ch>
2022-04-22 15:37:50 +0200

    Linters

--- spec/system/health_spec.rb
+++ spec/system/health_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Health check' do
-  it 'should check if the app is ok and connected to a database' do
+  it 'checks if the app is ok and connected to a database' do
     visit '/home/check'
     expect(page).to have_content('1+2=3')
   end
