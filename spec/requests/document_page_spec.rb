4df2f2943c0dd79043606c4043aa7251ed7781c2
nicole schmidlin <nicole.schmidlin@renuo.ch>
2022-05-18 13:41:04 +0200

    Update request spec

--- spec/requests/document_page_spec.rb
+++ spec/requests/document_page_spec.rb
@@ -4,8 +4,19 @@ require 'rails_helper'
 
 RSpec.describe 'DocumentPages', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/document_page'
+    let(:params) do
+      {
+        sr_number: 101,
+        language_tag: 'de'
+      }
+    end
+
+    it 'raises an error without params' do
+      expect { get document_page_url }.to raise_error(NoMethodError)
+    end
+
+    it 'return http success with correct params' do
+      get '/document_page', params: { sr_number: '818.102', language_tag: 'de' }
       expect(response).to have_http_status(:success)
     end
   end
