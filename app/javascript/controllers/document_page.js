4afd54acd90659ea15ac884fd68c6a627093003e
nicole schmidlin <nicole.schmidlin@renuo.ch>
2022-05-19 15:24:33 +0200

    Correct limit selection of checkboxes

--- app/javascript/controllers/document_page.js
+++ app/javascript/controllers/document_page.js
@@ -7,6 +7,20 @@ export default class extends Controller {
 
     connect(){
         this.showUnifield();
+
+        // copied from https://stackoverflow.com/questions/43456868/javascript-limit-selected-checkboxes-to-2
+        let checkboxes = document.querySelectorAll(".revisionCheckbox");
+        let max = 2;
+        for (let i = 0; i < checkboxes.length; i++) {
+            checkboxes[i].onclick = selectiveCheck;
+        }
+
+        function selectiveCheck(event) {
+            console.log('hi')
+            let checkedCheckboxes = document.querySelectorAll(".revisionCheckbox:checked");
+            if (checkedCheckboxes.length >= max + 1)
+                return false;
+        }
     }
 
     showUnifield(){
